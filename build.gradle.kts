4a654ed64398f01d168ded21ee6cae8f3dda42ce
PalmDevs <git@palmdevs.me>
2025-12-07 01:48:00 +0700

    chore: use gradle plugins instead of buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,9 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 import org.gradle.api.tasks.compile.JavaCompile
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-buildscript {
-    val kotlin_version: String by extra
-
-    repositories {
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
+plugins {
+    kotlin("jvm") apply false
 }
 
 subprojects {
