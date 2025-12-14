31a5e123705d4b8b61d807962f9a58038db00e2b
PalmDevs <git@palmdevs.me>
2025-12-15 00:26:52 +0700

    fix(api): only enforce limits on non-notifications route
    Enforcing on notifications route seems to break the server completely, freezing the server's `WebSocketSession.send()`.
    
    The server can still receive data from the client, which is odd.

--- api/src/main/kotlin/th/ac/bodin2/electives/api/HTTP.kt
+++ api/src/main/kotlin/th/ac/bodin2/electives/api/HTTP.kt
@@ -5,7 +5,9 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.bodylimit.*
 import io.ktor.server.plugins.conditionalheaders.*
 import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.request.*
 import io.ktor.server.resources.*
+import th.ac.bodin2.electives.utils.KiB
 import th.ac.bodin2.electives.utils.isDev
 import th.ac.bodin2.electives.utils.isTest
 import th.ac.bodin2.electives.utils.requireEnvNonBlank
@@ -14,7 +16,16 @@ fun Application.configureHTTP() {
     install(Resources)
 
     install(RequestBodyLimit) {
-        bodyLimit { 4 * 1024 * 1024L } // 4 MiB
+        bodyLimit { call ->
+            when (val path = call.request.path()) {
+                "/auth" -> 2.KiB
+                "/notifications" -> Long.MAX_VALUE
+                else -> {
+                    if (path.startsWith("/users")) 1.KiB
+                    else 0
+                }
+            }
+        }
     }
 
     install(CORS) {
