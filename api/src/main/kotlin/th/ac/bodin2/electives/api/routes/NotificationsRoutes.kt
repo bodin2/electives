7d1f09dc37ce27ebb6552abaf8fcc40844b2d4e6
PalmDevs <git@palmdevs.me>
2025-12-16 02:02:00 +0700

    fix(api): notifications improvements and fixes
    - Separated out logic to NotificationsService
    - Single Channel for safe concurrent sends
    - Launch less coroutines for updates
    - Added logs
    - Handle slow clients better

--- api/src/main/kotlin/th/ac/bodin2/electives/api/routes/NotificationsRoutes.kt
+++ api/src/main/kotlin/th/ac/bodin2/electives/api/routes/NotificationsRoutes.kt
@@ -3,154 +3,32 @@ package th.ac.bodin2.electives.api.routes
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
-import io.ktor.util.collections.*
-import io.ktor.websocket.*
-import kotlinx.coroutines.*
-import org.jetbrains.exposed.sql.transactions.transaction
-import th.ac.bodin2.electives.api.services.SubjectSelectionUpdateListener
-import th.ac.bodin2.electives.api.services.UsersService
-import th.ac.bodin2.electives.api.utils.*
-import th.ac.bodin2.electives.db.Elective
-import th.ac.bodin2.electives.proto.api.NotificationsService
-import th.ac.bodin2.electives.proto.api.NotificationsService.Envelope.PayloadCase
-import th.ac.bodin2.electives.proto.api.NotificationsServiceKt.acknowledged
-import th.ac.bodin2.electives.proto.api.NotificationsServiceKt.bulkSubjectEnrollmentUpdate
-import th.ac.bodin2.electives.proto.api.NotificationsServiceKt.envelope
-import th.ac.bodin2.electives.proto.api.NotificationsServiceKt.subjectEnrollmentUpdate
-import th.ac.bodin2.electives.utils.setInterval
-import java.util.concurrent.ConcurrentHashMap
+import th.ac.bodin2.electives.api.services.NotificationsService
+import th.ac.bodin2.electives.api.services.NotificationsService.handleConnection
+import th.ac.bodin2.electives.api.utils.authenticated
+import th.ac.bodin2.electives.api.utils.authenticatedRoutes
+import th.ac.bodin2.electives.utils.KiB
+import th.ac.bodin2.electives.utils.isDev
+import th.ac.bodin2.electives.utils.isTest
 import kotlin.time.Duration.Companion.seconds
-
-val clients = ConcurrentSet<WebSocketServerSession>()
-
-// UserId -> Map<ElectiveId, List<SubjectId>>
-val clientSubscriptions = ConcurrentHashMap<WebSocketServerSession, MutableMap<Int, List<Int>>>()
-val clientCleanups = ConcurrentHashMap<WebSocketServerSession, () -> Unit>()
-
-private const val MAX_SUBJECTS_SUBSCRIPTIONS_PER_CLIENT = 5
-private val BULK_UPDATE_INTERVAL = 5.seconds
 
 fun Application.registerNotificationsRoutes() {
     install(WebSockets) {
         pingPeriod = 15.seconds
         timeout = 15.seconds
-        // 16 KB
-        maxFrameSize = 16384
-        masking = false
+        maxFrameSize = 16.KiB
+        masking = !isDev && !isTest
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
-    GlobalScope.launch {
-        setInterval(BULK_UPDATE_INTERVAL) {
-            if (clients.isEmpty()) return@setInterval
-
-            val updates = transaction {
-                Elective.getAllIds().map { electiveId ->
-                    val enrolledCounts = Elective.getSubjectsEnrolledCounts(electiveId)
-
-                    envelope {
-                        bulkSubjectEnrollmentUpdate = bulkSubjectEnrollmentUpdate {
-                            this.electiveId = electiveId
-                            this.subjectEnrolledCounts.putAll(enrolledCounts)
-                        }
-                    }
-                }
-            }
-
-            for (client in clients)
-                for (update in updates) client.send(update)
-        }
-    }
+    NotificationsService.startBulkUpdateLoop()
 
     routing {
         authenticatedRoutes {
             webSocket("/notifications") {
-                clients.add(this)
-
-                try {
-                    for (frame in incoming) handleFrame(frame)
-                } finally {
-                    // Cleanup on disconnect
-                    clients.remove(this)
-                    clientSubscriptions.remove(this)
-                    clientCleanups.remove(this)?.invoke()
+                authenticated { userId ->
+                    handleConnection(userId)
                 }
             }
         }
     }
 }
-
-private suspend fun WebSocketServerSession.handleFrame(frame: Frame) {
-    if (frame !is Frame.Binary) return badFrame()
-
-    val envelope = frame.parseOrNull<NotificationsService.Envelope>() ?: return badFrame()
-
-    when (envelope.payloadCase) {
-        PayloadCase.SUBJECT_ENROLLMENT_UPDATE_SUBSCRIPTION_REQUEST -> {
-            val subscriptions = envelope
-                .subjectEnrollmentUpdateSubscriptionRequest
-                .subscriptionsMap
-
-            if (subscriptions
-                    .map { (_, subscription) -> subscription.subjectIdsCount }
-                    .sum() > MAX_SUBJECTS_SUBSCRIPTIONS_PER_CLIENT
-            )
-                return badFrame("Exceeded maximum subject subscriptions per client: $MAX_SUBJECTS_SUBSCRIPTIONS_PER_CLIENT")
-
-            subscriptions.forEach { (electiveId, subjectIds) ->
-                clientSubscriptions
-                    .getOrPut(this) { mutableMapOf() }[electiveId] =
-                    subjectIds.subjectIdsList
-            }
-
-            acknowledge(envelope)
-
-            // ? In the future, if we need more cleanups, simply wrap it in one big lambda
-            // Send the immediate updates the client requested
-            clientCleanups[this] = sendClientEnrollmentUpdates()
-        }
-
-        // Server payloads
-        PayloadCase.SUBJECT_ENROLLMENT_UPDATE, PayloadCase.BULK_SUBJECT_ENROLLMENT_UPDATE, PayloadCase.ACKNOWLEDGED ->
-            return badFrame()
-
-        // Invalid payload
-        PayloadCase.PAYLOAD_NOT_SET -> return badFrame()
-    }
-}
-
-suspend fun WebSocketSession.acknowledge(envelope: NotificationsService.Envelope) {
-    send(envelope {
-        messageId = envelope.messageId
-        acknowledged = acknowledged {}
-    })
-}
-
-private fun WebSocketServerSession.sendClientEnrollmentUpdates(): () -> Unit {
-    val subscriptions = clientSubscriptions[this]
-        ?: throw IllegalStateException("Client subscriptions $clientSubscriptions not found")
-
-    val listener: SubjectSelectionUpdateListener = { enrolledCount ->
-        scope.launch {
-            send(envelope {
-                subjectEnrollmentUpdate = subjectEnrollmentUpdate {
-                    this.electiveId = electiveId
-                    this.subjectId = subjectId
-                    this.enrolledCount = enrolledCount
-                }
-            })
-        }
-    }
-
-    for ((electiveId, subjectIds) in subscriptions)
-        for (subjectId in subjectIds)
-            UsersService.subscribeToSubjectSelections(electiveId, subjectId, listener)
-
-    return {
-        for ((electiveId, subjectIds) in subscriptions)
-            for (subjectId in subjectIds)
-                UsersService.unsubscribeFromSubjectSelections(electiveId, subjectId, listener)
-    }
-}
-
-private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
---
+++ api/src/main/kotlin/th/ac/bodin2/electives/api/services/NotificationsService.kt
@@ -0,0 +1,246 @@
+package th.ac.bodin2.electives.api.services
+
+import io.ktor.server.plugins.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.slf4j.LoggerFactory
+import th.ac.bodin2.electives.api.utils.badFrame
+import th.ac.bodin2.electives.api.utils.parseOrNull
+import th.ac.bodin2.electives.api.utils.send
+import th.ac.bodin2.electives.db.Elective
+import th.ac.bodin2.electives.proto.api.NotificationsService
+import th.ac.bodin2.electives.proto.api.NotificationsService.Envelope.PayloadCase
+import th.ac.bodin2.electives.proto.api.NotificationsServiceKt.acknowledged
+import th.ac.bodin2.electives.proto.api.NotificationsServiceKt.bulkSubjectEnrollmentUpdate
+import th.ac.bodin2.electives.proto.api.NotificationsServiceKt.envelope
+import th.ac.bodin2.electives.proto.api.NotificationsServiceKt.subjectEnrollmentUpdate
+import th.ac.bodin2.electives.utils.setInterval
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.time.Duration.Companion.seconds
+
+private val bulkUpdateScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+// Map<ElectiveId, Envelope (latest)>
+private val bulkUpdates = MutableStateFlow<Map<Int, NotificationsService.Envelope>>(emptyMap())
+
+object NotificationsService {
+    private val logger = LoggerFactory.getLogger(NotificationsService::class.java)
+    private val connectionCount = AtomicInteger(0)
+
+    // @TODO: Load from config
+    private const val MAX_SUBJECTS_SUBSCRIPTIONS_PER_CLIENT = 5
+
+    // @TODO: Load from config
+    private val BULK_UPDATE_INTERVAL = 5.seconds
+
+    private val updateScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    fun startBulkUpdateLoop() = bulkUpdateScope.launch {
+        setInterval(BULK_UPDATE_INTERVAL) {
+            if (connectionCount.get() == 0) return@setInterval
+
+            val startMs = System.currentTimeMillis()
+            logger.info("Sending bulk enrollment updates...")
+
+            val updates = transaction {
+                Elective.getAllIds().map { electiveId ->
+                    val enrolledCounts =
+                        Elective.getSubjectsEnrolledCounts(electiveId)
+
+                    envelope {
+                        bulkSubjectEnrollmentUpdate =
+                            bulkSubjectEnrollmentUpdate {
+                                this.electiveId = electiveId
+                                this.subjectEnrolledCounts
+                                    .putAll(enrolledCounts)
+                            }
+                    }
+                }
+            }
+
+            bulkUpdates.update { current ->
+                current + updates.associateBy { it.bulkSubjectEnrollmentUpdate.electiveId }
+            }
+
+            logger.info("Finished sending bulk updates in ${System.currentTimeMillis() - startMs}ms")
+        }
+    }
+
+    suspend fun WebSocketServerSession.handleConnection(userId: Int) {
+        logger.info("Client connected, user: $userId, IP: ${call.request.origin.remoteHost}")
+
+        val connection = ClientConnection(
+            userId = userId,
+            session = this,
+            subscriptions = ConcurrentHashMap(),
+            cleanups = ConcurrentHashMap.newKeySet(),
+            parentScope = updateScope,
+        )
+
+        connectionCount.incrementAndGet()
+
+        try {
+            for (frame in incoming) connection.handleFrame(frame)
+        } finally {
+            connection.close()
+            connectionCount.decrementAndGet()
+        }
+    }
+
+    private suspend fun ClientConnection.handleFrame(frame: Frame) = with(session) {
+        if (frame !is Frame.Binary) return badFrame()
+
+        val envelope = frame.parseOrNull<NotificationsService.Envelope>() ?: return badFrame()
+
+        when (envelope.payloadCase) {
+            PayloadCase.SUBJECT_ENROLLMENT_UPDATE_SUBSCRIPTION_REQUEST -> {
+                val subscriptions = envelope
+                    .subjectEnrollmentUpdateSubscriptionRequest
+                    .subscriptionsMap
+
+                if (subscriptions
+                        .map { (_, subscription) -> subscription.subjectIdsCount }
+                        .sum() > MAX_SUBJECTS_SUBSCRIPTIONS_PER_CLIENT
+                )
+                    return badFrame("Exceeded maximum subject subscriptions per client: $MAX_SUBJECTS_SUBSCRIPTIONS_PER_CLIENT")
+
+                subscriptions.forEach { (electiveId, subjectIds) ->
+                    this@handleFrame.subscriptions
+                        .getOrPut(electiveId) { ConcurrentHashMap.newKeySet() } += subjectIds.subjectIdsList
+                }
+
+                acknowledge(envelope)
+                logger.info("Client subscriptions updated, user: $userId")
+
+                // Cleanup previous enrollment updates
+                for (cleanup in cleanups) runCatching { cleanup() }.onFailure { logger.error("Error during elective enrollment subscription update:", it) }
+                cleanups.clear()
+                // Send the immediate updates the client requested
+                cleanups += sendEnrollmentUpdates()
+            }
+
+            // Server payloads
+            PayloadCase.SUBJECT_ENROLLMENT_UPDATE, PayloadCase.BULK_SUBJECT_ENROLLMENT_UPDATE, PayloadCase.ACKNOWLEDGED ->
+                return badFrame()
+
+            // Invalid payload
+            PayloadCase.PAYLOAD_NOT_SET -> return badFrame()
+        }
+
+        // Make sure it returns something
+        return@with
+    }
+
+    private fun ClientConnection.sendEnrollmentUpdates(): () -> Unit {
+        val listener: SubjectSelectionUpdateListener = { enrolledCount ->
+            trySend(envelope {
+                subjectEnrollmentUpdate = subjectEnrollmentUpdate {
+                    this.electiveId = electiveId
+                    this.subjectId = subjectId
+                    this.enrolledCount = enrolledCount
+                }
+            })
+        }
+
+        for ((electiveId, subjectIds) in subscriptions)
+            for (subjectId in subjectIds)
+                UsersService.subscribeToSubjectSelections(electiveId, subjectId, listener)
+
+        return {
+            for ((electiveId, subjectIds) in subscriptions)
+                for (subjectId in subjectIds)
+                    UsersService.unsubscribeFromSubjectSelections(electiveId, subjectId, listener)
+        }
+    }
+
+    private suspend fun ClientConnection.acknowledge(envelope: NotificationsService.Envelope) {
+        send(envelope {
+            messageId = envelope.messageId
+            acknowledged = acknowledged {}
+        })
+    }
+}
+
+class ClientConnection(
+    val userId: Int,
+    val session: WebSocketServerSession,
+    // Map<ElectiveId, List<SubjectId>>
+    val subscriptions: ConcurrentHashMap<Int, ConcurrentHashMap.KeySetView<Int, Boolean>>,
+    val cleanups: ConcurrentHashMap.KeySetView<() -> Unit, Boolean>,
+    parentScope: CoroutineScope,
+) {
+    companion object {
+        private const val DROPPED_LIMIT = 32
+        private val logger = LoggerFactory.getLogger(ClientConnection::class.java)
+
+        private fun ClientConnection.handleUndeliveredElement() {
+            @OptIn(DelicateCoroutinesApi::class)
+            if (!outgoing.isClosedForSend) {
+                val dropped = droppedCount.incrementAndGet()
+                logger.warn("Dropped envelope for user: $userId, dropped so far: $dropped")
+
+                if (dropped >= DROPPED_LIMIT) {
+                    logger.error("Dropped envelope limit reached for user: $userId, closing connection")
+                    scope.cancel()
+                }
+            }
+        }
+    }
+
+    private val scope = CoroutineScope(parentScope.coroutineContext + SupervisorJob())
+    private val droppedCount = AtomicInteger(0)
+
+    // One channel per client to buffer outgoing messages, to prevent concurrent sends/writes
+    private val outgoing = Channel<NotificationsService.Envelope>(
+        16,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST,
+        onUndeliveredElement = { handleUndeliveredElement() }
+    )
+
+    init {
+        val job = scope.coroutineContext[Job]!!
+
+        job.invokeOnCompletion {
+            scope.launch(NonCancellable) {
+                runCatching {
+                    session.close()
+                }.onFailure {
+                    logger.error("Error closing WebSocket session for user: $userId", it)
+                }
+            }
+
+            for (cleanup in cleanups) runCatching { cleanup() }.onFailure { logger.error("Error during ClientConnection close:", it) }
+        }
+
+
+        scope.launch {
+            for (msg in outgoing) session.send(msg)
+        }
+
+        scope.launch {
+            var last: Map<Int, NotificationsService.Envelope> = emptyMap()
+
+            bulkUpdates.collect { current ->
+                // ? This is generally fine for small numbers of electives.
+                // ? If this becomes a bottleneck, move to per‑elective StateFlow.
+                for ((id, update) in current)
+                    if (last[id] != update) send(update)
+
+                last = current
+            }
+        }
+    }
+
+    suspend fun send(msg: NotificationsService.Envelope) = outgoing.send(msg)
+
+    fun trySend(msg: NotificationsService.Envelope) = outgoing.trySend(msg)
+
+    fun close() = scope.cancel()
+}
