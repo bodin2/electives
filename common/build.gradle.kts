cbfa14a7517cd327d84773bcc4f494f7aef897fd
PalmDevs <git@palmdevs.me>
2026-01-01 18:49:55 +0700

    fix(common): expose SQLite JDBC dependency

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     api(libs.exposed.jdbc)
     api(libs.exposed.java.time)
 
-    implementation(libs.sqlite.jdbc)
+    api(libs.sqlite.jdbc)
     implementation(libs.dotenv.kotlin)
 
     // @TODO: Use the version without prebuilt binaries and include setup instructions
