1145805d93094d6af22ffd1509884f012deaa627
PalmDevs <git@palmdevs.me>
2026-01-31 22:41:59 +0700

    fix(common/proto/api): remove `NotificationsService.MessageType`

--- web/src/entry-client.tsx
+++
@@ -1,5 +0,0 @@
-// @refresh reload
-import { mount, StartClient } from '@solidjs/start/client'
-
-// biome-ignore lint/style/noNonNullAssertion: This is a required element in the HTML
-mount(() => <StartClient />, document.getElementById('app')!)
