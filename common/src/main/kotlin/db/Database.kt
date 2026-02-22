42e7f618c53e661b19ef6a943085a3ec28d7187a
PalmDevs <git@palmdevs.me>
2026-02-22 18:58:11 +0700

    chore(common/db/Database): rename imports to reduce confusion

--- common/src/main/kotlin/db/Database.kt
+++ common/src/main/kotlin/db/Database.kt
@@ -1,15 +1,13 @@
 package th.ac.bodin2.electives.db
 
-import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import th.ac.bodin2.electives.db.models.*
+import org.jetbrains.exposed.v1.jdbc.Database as ExposedDatabase
 
 object Database {
-    fun init(url: String, driver: String) = init(url, driver) {}
-
-    fun init(url: String, driver: String, setup: org.jetbrains.exposed.v1.jdbc.Database.() -> Unit): Database {
-        val db = Database.connect(url, driver).apply(setup)
+    fun init(url: String, driver: String, setup: ExposedDatabase.() -> Unit): ExposedDatabase {
+        val db = ExposedDatabase.connect(url, driver).apply(setup)
 
         // Create tables if they do not exist
         transaction { SchemaUtils.create(*tables.toTypedArray()) }
