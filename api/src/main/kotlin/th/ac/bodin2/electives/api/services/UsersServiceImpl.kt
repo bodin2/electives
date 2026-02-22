4f56fa4615c6e72b9c7b7e53d8fd6c3916206b2c
PalmDevs <git@palmdevs.me>
2026-02-22 16:55:35 +0700

    fix(api/services/UsersServiceImpl): orderBy at correct query in `getStudents`

--- api/src/main/kotlin/th/ac/bodin2/electives/api/services/UsersServiceImpl.kt
+++ api/src/main/kotlin/th/ac/bodin2/electives/api/services/UsersServiceImpl.kt
@@ -192,6 +192,7 @@ class UsersServiceImpl(val config: Config) : UsersService {
         return transaction {
             val studentIds = (Students innerJoin Users)
                 .select(Students.id)
+                .orderBy(Students.id)
                 .limit(PAGE_SIZE)
                 .offset(offset)
                 .map { it[Students.id].value }
@@ -208,7 +209,6 @@ class UsersServiceImpl(val config: Config) : UsersService {
             ((Students innerJoin Users)
                 .select(Students.columns + userInfoFields)
                 .where { Students.id inList studentIds }
-                .orderBy(Students.id)
                 .map { row ->
                     Student(
                         Student.Reference.from(row),
