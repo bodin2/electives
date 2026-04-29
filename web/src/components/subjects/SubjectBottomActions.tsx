0cfa083eaaca3c39ca3d916295a67501cbd446f3
PalmDevs <git@palmdevs.me>
2026-04-29 21:22:07 +0700

    fix(web): disable save button when subject data is invalid

--- web/src/components/subjects/SubjectBottomActions.tsx
+++ web/src/components/subjects/SubjectBottomActions.tsx
@@ -1,5 +1,6 @@
 import SaveIcon from '@iconify-icons/mdi/content-save'
 import { type Component, Show } from 'solid-js'
+import { SubjectTag } from '../../api'
 import { useI18n } from '../../providers/I18nProvider'
 import { useScrollData } from '../../providers/ScrollDataProvider'
 import { Button } from '../Button'
@@ -11,6 +12,10 @@ export default function SubjectBottomActions(props: { extraContent?: Component }
     const ctx = useSubjectInfoContext()
     const { string } = useI18n()
     const scrollData = useScrollData()
+
+    const isValid = () => {
+        return ctx.subject.tag !== SubjectTag.UNRECOGNIZED
+    }
 
     return (
         <VStack
@@ -24,7 +29,13 @@ export default function SubjectBottomActions(props: { extraContent?: Component }
             {/* @ts-expect-error: Wrong types */}
             <Show when={props.extraContent}>{Content => <div class={styles.actionButton}>{<Content />}</div>}</Show>
             <Show when={ctx.onSave && ctx.editable}>
-                <Button icon={SaveIcon} class={styles.actionButton} size="m" onClick={() => ctx.onSave?.()}>
+                <Button
+                    icon={SaveIcon}
+                    class={styles.actionButton}
+                    size="m"
+                    onClick={() => ctx.onSave?.()}
+                    disabled={!isValid()}
+                >
                     {string.SAVE()}
                 </Button>
             </Show>
